7d1ce2d013e6d05e33f0a971b41ef3fadbe7ac5e
Kiber <Koloc04@mail.ru>
2026-04-12 17:20:38 +0300

    Fix views, forms, add runtime.txt

--- vet_system/urls.py
+++ vet_system/urls.py
@@ -1,12 +1,11 @@
+from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from core import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('add/', views.add_visit, name='add_visit'),
     path('pet/<int:pk>/', views.pet_detail, name='pet_detail'),
     path('import/', views.import_data, name='import_data'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
